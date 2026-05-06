d2ed607e21bae466d6dcd49df471da959d8b4131
tharun-naru <narutharunreddy@gmail.com>
2026-05-06 21:30:21 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,7 +8,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
         server.createContext("/", exchange -> {
-            String response = "Hello World ";
+            String response = "Hello World webhook";
             exchange.sendResponseHeaders(200, response.length());
             OutputStream os = exchange.getResponseBody();
             os.write(response.getBytes());
